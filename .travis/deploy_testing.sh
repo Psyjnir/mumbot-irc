72ba8e4c432b9db42615a154cae36fe8d721825d
Charles Powell <cbpowell@gmail.com>
2016-03-12 16:27:43 -0700

    Fix missing if/then, only build if PR is targeting master to avoid double-deployment.

--- .travis/deploy_testing.sh
+++ .travis/deploy_testing.sh
@@ -6,9 +6,13 @@ ssh-add .travis/travis_rsa
 git config --global push.default simple
 git remote add testing "${TEST_LOCATION}"
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   exit 0
 fi
 
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+  exit 0
+fi
+  
 echo "Pushing to Test"
 git push testing master --force
