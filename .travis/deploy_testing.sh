6a071e9de44705ef53bdc4bb937b21da0b526c3a
Charles Powell <cbpowell@gmail.com>
2017-04-18 18:37:21 -0700

    Reorder deployment check.

--- .travis/deploy_testing.sh
+++ .travis/deploy_testing.sh
@@ -1,15 +1,15 @@
 #!/bin/bash 
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo "NOTE: Main or Pull request merge branch, stopping."
+  exit 0
+fi
+
 eval "$(ssh-agent -s)"
 chmod 600 .travis/travis_rsa
 ssh-add .travis/travis_rsa
 
 git config --global push.default simple
 git remote add testing "${TEST_LOCATION}"
-
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-  echo "NOTE: Main or Pull request merge branch, stopping."
-  exit 0
-fi
 
 echo "NOTE: Pushing $TRAVIS_BRANCH to Test!"
 git push testing HEAD:master --force
