e0ba24715e60bb6dea5cfcdf6b8c8bf0323d5ddc
Charles Powell <cbpowell@gmail.com>
2026-03-08 18:38:18 -0700

    Remove extra logging

--- scripts/psyjnir.js
+++ scripts/psyjnir.js
@@ -127,11 +127,7 @@ module.exports = function(robot) {
 		  return Math.floor(Math.random() * (max - min + 1)) + min; // The maximum is inclusive and the minimum is inclusive
     }
 
-    console.log(msg);
-
     var dice = msg.message.text.split(" ")[1];
-
-    console.log(dice);
 
 		if(dice)
 		{
